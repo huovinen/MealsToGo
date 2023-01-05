9d70d823c00e712f7cc7e2edd79a351f825739a2
Emil Huovinen <emil.huo@gmail.com>
2023-01-05 18:09:46 +0100

    Home screen layout test

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{alignItems: 'center', flex: 1, justifyContent: 'center'}}>
-      <Text>React Native blank canvas</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchBar}>
+      <Text>Search</Text>
+      </View>
+      <View style={styles.list}>
+      <Text>List</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+
+  },
+  searchBar: {
+    padding: 10,
+    backgroundColor: 'green'
+  },
+  list: {
+    flexGrow: 1,
+    padding: 10,
+    backgroundColor: 'blue'
+  }
 });
