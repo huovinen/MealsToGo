eec3dd43bba9a259179bb7e2078fb37864304092
Emil Huovinen <emil.huo@gmail.com>
2023-01-05 17:38:23 +0100

    initial clean up

--- App.js
+++ App.js
@@ -3,18 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{alignItems: 'center', flex: 1, justifyContent: 'center'}}>
+      <Text>React Native blank canvas</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
