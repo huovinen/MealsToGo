26b743d739f0d7419cb84718bcf03859d8ad37fc
Emil Huovinen <emil.huo@gmail.com>
2023-01-13 17:19:36 +0100

    mock api and data

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { SafeArea } from "./src/utility/safe-area.component";
 import { Ionicons } from "@expo/vector-icons";
 
+import { restaurantsRequest } from "./src/services/restaurants/restaurants.service";
+
 const Tab = createBottomTabNavigator();
 
 const TAB_ICON = {
