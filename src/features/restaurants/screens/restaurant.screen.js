26b98af7262ffdbde59e407f1f8ef4cc74f53044
Emil Huovinen <emil.huo@gmail.com>
2023-01-17 16:23:01 +0100

    set up loading state

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,7 +1,13 @@
 import React, { useContext } from "react";
-import { Platform, StatusBar, SafeAreaView, FlatList } from "react-native";
+import {
+  Platform,
+  StatusBar,
+  SafeAreaView,
+  FlatList,
+  View,
+} from "react-native";
 import styled from "styled-components/native";
-import { Searchbar } from "react-native-paper";
+import { Searchbar, ActivityIndicator, Colors } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { RestaurantCard } from "../components/restaurant-info-card-styles";
 import { SafeArea } from "../../../utility/safe-area.component";
@@ -23,10 +29,29 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading
+            size={50}
+            animating={true}
+            color={(props) => props.theme.colors.brand.primary}
+          />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Search placeholder="Search" />
       </SearchContainer>
